73aa388d363cf6ea2dc73ba1ac34d7b6a1849d37
a186r <hi.aewalle@gmail.com>
2018-05-14 11:46:14 +0800

    TokenRegistry comments

--- packages/contracts/src/contracts/current/protocol/TokenRegistry/TokenRegistry.sol
+++ packages/contracts/src/contracts/current/protocol/TokenRegistry/TokenRegistry.sol
@@ -20,6 +20,7 @@ pragma solidity ^0.4.11;
 
 import { Ownable_v1 as Ownable } from "../../../previous/Ownable/Ownable_v1.sol";
 
+// 存储与ERC20相关的元数据
 /// @title Token Registry - Stores metadata associated with ERC20 tokens. See ERC22 https://github.com/ethereum/EIPs/issues/22
 /// @author Amir Bandeali - <amir@0xProject.com>, Will Warren - <will@0xProject.com>
 contract TokenRegistry is Ownable {
